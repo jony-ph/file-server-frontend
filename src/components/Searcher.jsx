79387f1e2059fe9d3bd9ccf18a0e6e3cde7619b0
Jonathan Perez Hernandez <jonathan_perez_hdz@hotmail.com>
2022-05-18 20:24:25 -0500

    Searcher design

---
+++ src/components/Searcher.jsx
@@ -0,0 +1,22 @@
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+
+import { BsSearch } from "react-icons/bs";
+
+const Searcher = () => {
+  return ( 
+    <InputGroup className="my-5">
+      <FormControl
+        placeholder="Carpeta o archivo"
+        aria-label="Carpeta o archivo"
+        aria-describedby="basic-addon2"
+      />
+      <Button variant="secondary" id="button-addon2">
+        <BsSearch />
+      </Button>
+    </InputGroup>
+  );
+}
+ 
+export default Searcher;
