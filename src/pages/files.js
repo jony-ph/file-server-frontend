0ad3df2c78dee40fdc99f013c4e7b02db0ddc114
Jonathan Perez Hernandez <jonathan_perez_hdz@hotmail.com>
2022-05-18 20:30:18 -0500

    Pages.js: I implemented buttons, folder, searcher components and create general style for the pages.

--- src/pages/files.js
+++ src/pages/files.js
@@ -1,7 +1,27 @@
+import ButtonCreate from "../components/ButtonCreate";
+import ButtonUpload from "../components/ButtonUpload";
+import Searcher from "../components/Searcher";
+
+import Stack from "react-bootstrap/Stack";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import ButtonOrder from "../components/ButtonOrder";
+
 const Files = () => {
   return ( 
     <div className="main container">
       <h2 className="text-center pt-5">Files</h2>
+      <Searcher />
+      <Stack direction="horizontal">
+        <ButtonUpload />
+        <ButtonCreate />
+        <ButtonOrder className="ms-auto" />
+      </Stack>
+
+      <Container className="my-5">
+        <Row className="text-center" xs={2} sm={3} md={4} lg={5} xl={6} xxl={8}>
+        </Row>
+      </Container>
     </div>
   );
 }
