2591ca9f7d560160410f58084def02a394b4fc75
Jonathan Perez Hernandez <jonathan_perez_hdz@hotmail.com>
2022-06-01 21:29:20 -0500

    A single component to render files and folders

---
+++ src/components/FolderFile.jsx
@@ -0,0 +1,41 @@
+import FileMenu from './FileMenu';
+import { Link } from 'react-router-dom';
+
+import { Col } from 'react-bootstrap';
+import { FaFolder } from 'react-icons/fa';
+import { BsFileEarmarkArrowDownFill } from 'react-icons/bs';
+
+import styles from '../styles/folderFile.module.css'
+
+const FolderFile = ({ path, item }) => {
+
+  let pathname = path;
+  if (item.isFile)
+    pathname = '/preview' + path;
+
+  const name = path.substring(path.lastIndexOf('/') + 1);
+  let displayName = name;
+
+  if (name.length >= 25)
+    displayName = name.slice(0, 25) + '...';
+
+  return ( 
+    <Col className="user-select-none d-flex">
+      <div className="flex-grow-1">
+        <Link to={pathname} className={styles.link}>
+          {
+            item.isFile
+            ? <BsFileEarmarkArrowDownFill className={styles.file} />
+            : <FaFolder className={styles.folder} />
+          }
+          <div className="text-center">
+            {displayName}
+          </div>
+        </Link>
+      </div>
+      <FileMenu path={path} name={name} item={item} />
+    </Col>
+  );
+}
+ 
+export default FolderFile;
