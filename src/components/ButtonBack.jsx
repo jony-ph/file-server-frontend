f0114596c7f93bb68d55d545dca21ab00ae930e2
Jonathan Perez Hernandez <jonathan_perez_hdz@hotmail.com>
2022-06-01 21:13:55 -0500

    Implemented button to back to previous folder

---
+++ src/components/ButtonBack.jsx
@@ -0,0 +1,23 @@
+import { BiArrowBack } from 'react-icons/bi';
+
+import { Link, useLocation } from 'react-router-dom';
+
+
+const ButtonBack = () => {
+
+  const { pathname } = useLocation();
+
+  const currentPath = pathname.substring( pathname.lastIndexOf('/') )
+  const goBack = pathname.replace(currentPath, "");
+
+  if (pathname !== '/files')
+    return ( 
+      <div className="mx-3"> 
+          <Link to={goBack}>
+            <BiArrowBack size={30} color="white"/>
+          </Link>
+      </div>
+    );
+}
+ 
+export default ButtonBack;
