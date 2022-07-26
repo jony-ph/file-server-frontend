7cb5c3f5ce85c7fb56a4ae080a2c12dabbe54818
Jonathan Perez Hernandez <jonathan_perez_hdz@hotmail.com>
2022-07-26 01:11:10 -0500

    Added uuid in container files

--- src/components/ContainerFiles.jsx
+++ src/components/ContainerFiles.jsx
@@ -1,14 +1,18 @@
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-import { uuid } from "uuidv4";
 import FolderFile from "./FolderFile";
 
 function ContainerFiles({ items }) {
+	const uuid = () => {
+		const time = new Date.Now();
+		return time + Math.random;
+	};
+
 	return (
 		<Container className="py-5">
 			<Row className="text-center" xs={2} sm={3} md={4} lg={5} xl={6} xxl={8}>
 				{items.map((item) => (
-					<FolderFile key={uuid()} path={item.path} item={item} />
+					<FolderFile key={uuid} path={item.path} item={item} />
 				))}
 			</Row>
 		</Container>
