3787b53ac6a1b5d244f03750351edc65ae9e4b46
Jonathan Perez Hernandez <jonathan_perez_hdz@hotmail.com>
2022-07-26 01:33:36 -0500

    Change main message

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,10 @@ function Home() {
         Este es un proyecto propio para mostrar mis habilidades técnicas con
         React, en realidad funciona localmente pero por cuestiones de
         demonstración el proyecto lo subo directamente como un prototipo. Su
-        funcionamiento es el de un servidor de archivos local.
+        funcionamiento es el de un servidor de archivos local. Si no puedes
+        interactuar con los archivos quizá tengas un bloqueador que no te
+        permita ejecutar JavaScript, para el buen funcionamiento de la página
+        desactivalo por favor.
       </p>
     </div>
   );
